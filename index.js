d13116beb87f6eb0ac1e6d8d080dd2306b3ae1ec
nhanhthat <84766117+nhanhthat@users.noreply.github.com>
2023-09-16 20:36:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors([{
 
 db.connect()
 
-app.post('https://sansalesapsan.online/api/live_x', async (req, res) => {
+app.post('/api/live_x', async (req, res) => {
     try {
         const links = await Link.find({})
         let num = 0
