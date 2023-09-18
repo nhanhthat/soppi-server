bfec7f9e3f7c4488f3068d1a0b5cbb0fbddc2e85
nhanhthat <84766117+nhanhthat@users.noreply.github.com>
2023-09-19 01:03:38 +0700

    upload

--- index.js
+++ index.js
@@ -21,46 +21,50 @@ db.connect()
 
 
 app.post('/api/live_x', async (req, res) => {
-
-
-    try {
-      const links = await Link.find({})
-      let num = 0
-      links.forEach ((link) => {
-          if (link.queue > num) {
-              num = link.queue
-          }
-      })  
-      const link = new Link (req.body)
-      link.queue = num + 1
-      await link.save()
-      let isChanged = false;
-
-      let int = setInterval(async () => {
-          const links1 = await Link.find({})
-          for (const link of links1) {
-            if (link.queue == num + 1) {
-              if (link.status != 0) {
-                // await Link.deleteOne({ queue: num + 1 });
-                await Link.updateOne({queue : num + 1},{delete1 : true})
-                if (isChanged == false) {
-                  isChanged = true
-                  if (link.status == 1) {
-                    res.json({ status: 200, message: 'success' });
-                  } else if (link.status == -1){
-                      res.json({ status: 200, message: 'fail' });
-                  }
-                  clearInterval(int)
-                }
-              }
-              return;
+  let haha = setInterval(async () => {
+    const links1 = await Link.find({})
+    if (links1.length == 0) {
+      try {
+        const links = await Link.find({})
+        let num = 0
+        links.forEach ((link) => {
+            if (link.queue > num) {
+                num = link.queue
             }
-          }
-      }, 200)
+        })  
+        const link = new Link (req.body)
+        link.queue = num + 1
+        await link.save()
+        let isChanged = false;
 
-    } catch (error) {
-        res.json({status : 500})
+        let int = setInterval(async () => {
+            const links1 = await Link.find({})
+            for (const link of links1) {
+              if (link.queue == num + 1) {
+                if (link.status != 0) {
+                  // await Link.deleteOne({ queue: num + 1 });
+                  await Link.updateOne({queue : num + 1},{delete1 : true})
+                  if (isChanged == false) {
+                    isChanged = true
+                    if (link.status == 1) {
+                      res.json({ status: 200, message: 'success' });
+                    } else if (link.status == -1){
+                        res.json({ status: 200, message: 'fail' });
+                    }
+                    clearInterval(int)
+                  }
+                }
+                return;
+              }
+            }
+        }, 200)
+      } catch (error) {
+          res.json({status : 500})
+      } finally {
+        clearInterval(haha)
+      }
     }
+  }, 400) 
 })
 
 app.listen(port, () => {
